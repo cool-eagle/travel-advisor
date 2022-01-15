e2b8450a63ad0b21054ecfeb3c7e3f41ba782d65
yongri910 <yongri910@gmail.com>
2022-01-15 23:19:54 +0800

    to seve rapidapi

--- src/api/index.js
+++ src/api/index.js
@@ -6,20 +6,20 @@ const URL='https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
 
 export const getPlacesData=async(sw, ne)=>{
     try{
-        const {data: {data}}=await axios.get(URL, {
-            params: {
-              bl_latitude: sw.lat,
-              tr_latitude: ne.lat,
-              bl_longitude: sw.lng,
-              tr_longitude: ne.lng,
-            },
-            headers: {
-                'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-                'x-rapidapi-key': 'bbd4efed49msh2756d76c0cd53ecp133606jsn60748e1c3c48'
-            }
-          });
+        // const {data: {data}}=await axios.get(URL, {
+        //     params: {
+        //       bl_latitude: sw.lat,
+        //       tr_latitude: ne.lat,
+        //       bl_longitude: sw.lng,
+        //       tr_longitude: ne.lng,
+        //     },
+        //     headers: {
+        //         'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+        //         'x-rapidapi-key': 'bbd4efed49msh2756d76c0cd53ecp133606jsn60748e1c3c48'
+        //     }
+        //   });
 
-        return data;
+        // return data;
 
     } catch (error){
         console.log(error);
